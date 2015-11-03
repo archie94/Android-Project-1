91dcde6fe672f8baba6bb541f40f8abe2a3a87c7
Arka Prava Basu <arkaprava94@gmail.com>
2015-11-03 18:45:54 +0530

    Changes in memo_view.xml and MemoView,java

--- StudentTools/src/com/prizm/studenttools/MemoView.java
+++ StudentTools/src/com/prizm/studenttools/MemoView.java
@@ -2,15 +2,23 @@ package com.prizm.studenttools;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MemoView extends Activity  
 {
+	EditText memo ;
+	Button edit ; 
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) 
 	{
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.memo_view);
+		memo = (EditText)findViewById(R.id.memo_view_editText1);
+		edit = (Button)findViewById(R.id.memo_view_button1);
 		
 	}
 	

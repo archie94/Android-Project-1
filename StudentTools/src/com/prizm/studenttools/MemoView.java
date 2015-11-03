a9e4c527b05614f7d958e5f8157f73693dbfb516
Arka Prava Basu <arkaprava94@gmail.com>
2015-11-03 18:03:04 +0530

    Added MemoView.java

---
+++ StudentTools/src/com/prizm/studenttools/MemoView.java
@@ -0,0 +1,18 @@
+package com.prizm.studenttools;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MemoView extends Activity  
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) 
+	{
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+	}
+	
+
+}

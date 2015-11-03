9f6c94bd92c62d51e2e2e2c732f7e5b32718df38
Arka Prava Basu <arkaprava94@gmail.com>
2015-11-03 19:27:08 +0530

    EditText set to the selected memo

--- StudentTools/src/com/prizm/studenttools/MemoView.java
+++ StudentTools/src/com/prizm/studenttools/MemoView.java
@@ -2,14 +2,20 @@ package com.prizm.studenttools;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TextView.OnEditorActionListener;
 
 public class MemoView extends Activity  
 {
 	EditText memo ;
 	Button edit ; 
-	
+	String str;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) 
@@ -20,7 +26,61 @@ public class MemoView extends Activity
 		memo = (EditText)findViewById(R.id.memo_view_editText1);
 		edit = (Button)findViewById(R.id.memo_view_button1);
 		
+		Bundle bundle = getIntent().getExtras();
+		try
+		{
+			str = bundle.getString("memo");
+			memo.setText(str,TextView.BufferType.EDITABLE);
+			
+		}
+		catch(Exception e )
+		{
+			e.printStackTrace();
+		}
+		memo.setOnEditorActionListener(new OnEditorActionListener()
+		{
+
+			@Override
+			public boolean onEditorAction(TextView textView, int actionId, KeyEvent keyEvent) 
+			{
+				// TODO Auto-generated method stub
+				if(actionId == EditorInfo.IME_ACTION_DONE)
+				{
+					editMemo();
+					return true;
+				}
+				return false;
+			}
+
+			
+		});
+		
+		edit.setOnClickListener(new OnClickListener()
+		{
+
+			@Override
+			public void onClick(View arg0) 
+			{
+				// TODO Auto-generated method stub
+				editMemo();
+			}
+			
+		});
 	}
+	private void editMemo()
+	{
+		str=memo.getText().toString();
+	}
+	
+	
+	@Override
+	protected void onPause() 
+	{
+		// TODO Auto-generated method stub
+		super.onPause();
+		finish();
+	}
+	
 	
 
 }

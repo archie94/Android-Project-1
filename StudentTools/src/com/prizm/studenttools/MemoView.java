e6971120cdcbce3a4c464e5bb97528f79946009b
Arka Prava Basu <arkaprava94@gmail.com>
2015-11-04 20:04:41 +0530

    Removed un necessary button

--- StudentTools/src/com/prizm/studenttools/MemoView.java
+++ StudentTools/src/com/prizm/studenttools/MemoView.java
@@ -4,18 +4,14 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.KeyEvent;
-import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.inputmethod.EditorInfo;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.TextView.OnEditorActionListener;
 
 public class MemoView extends Activity  
 {
-	EditText memo ;
-	Button edit ; 
+	EditText memo ; 
 	String str;
 
 	@Override
@@ -25,7 +21,6 @@ public class MemoView extends Activity
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.memo_view);
 		memo = (EditText)findViewById(R.id.memo_view_editText1);
-		edit = (Button)findViewById(R.id.memo_view_button1);
 		
 		Bundle bundle = getIntent().getExtras();
 		try
@@ -55,17 +50,7 @@ public class MemoView extends Activity
 			
 		});
 		
-		edit.setOnClickListener(new OnClickListener()
-		{
-
-			@Override
-			public void onClick(View arg0) 
-			{
-				// TODO Auto-generated method stub
-				editMemo();
-			}
-			
-		});
+		
 	}
 	private void editMemo()
 	{

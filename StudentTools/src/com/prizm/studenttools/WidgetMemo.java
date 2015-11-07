cbe0f901d3c740bd241fb7fbc14c0ea2f809054e
Arka Prava Basu <arkaprava94@gmail.com>
2015-11-07 13:35:17 +0530

    Layout of widget improved

--- StudentTools/src/com/prizm/studenttools/WidgetMemo.java
+++ StudentTools/src/com/prizm/studenttools/WidgetMemo.java
@@ -1,6 +1,27 @@
 package com.prizm.studenttools;
 
-public class WidgetMemo 
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.widget.Toast;
+
+public class WidgetMemo extends AppWidgetProvider
 {
+	
+	@Override
+	public void onUpdate(Context context, AppWidgetManager appWidgetManager,int[] appWidgetIds) 
+	{
+		// TODO Auto-generated method stub
+		super.onUpdate(context, appWidgetManager, appWidgetIds);
+		
+	}
+
+	@Override
+	public void onDeleted(Context context, int[] appWidgetIds) 
+	{
+		// TODO Auto-generated method stub
+		super.onDeleted(context, appWidgetIds);
+		Toast.makeText(context, "Wiudget Removed", Toast.LENGTH_SHORT).show();
+	}
 
 }
